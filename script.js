5950335a7743481ee489f83cabbc74952c595027
kiddminecraft11 <kiddminecraft11@gmail.com>
2026-07-29 16:45:36 -0400

    first commit

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 // get references
+let textInput = document.getElementById('boxtext');
+let clickButton = document.querySelector('button');
+let screenDiv = document.querySelector('div');
+let bodyRef = document.querySelector('body');
 
 // set up onclick for clickButton
-
+clickButton.onclick = function(event) {
 // prevent the page from reloading
-
+event.preventDefault();
 // capture user input
-
+let userInput = textInput.value;
+console.log(userInput);
 // change backgroundColor based on userInput
-
+bodyRef.style.backgroundColor = userInput;
 // show userInput on the screen
-
+screenDiv.textContent = userInput;
 // Clear input
+}
